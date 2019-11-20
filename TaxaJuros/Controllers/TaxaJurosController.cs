3e27803de243c6995762322cf156b04d9c5874e1
Pericles Silva <periclesbps@gmail.com>
2019-11-20 18:57:24 -0300

    Upload project

---
+++ TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaxaJuros.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<double> TaxaJuros()
+        {
+            return 0.01;
+        }
+    }
+}
