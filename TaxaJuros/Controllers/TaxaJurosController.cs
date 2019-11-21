818fbc0ed7e3b03b419bbb91649470db36cb8187
Pericles Silva <periclesbps@gmail.com>
2019-11-21 11:42:27 -0300

    Ajustes

--- TaxaJuros/Controllers/TaxaJurosController.cs
+++ TaxaJuros/Controllers/TaxaJurosController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Serialization;
-using System.Text.Json.Serialization;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Dynamic;
 
 namespace TaxaJuros.Controllers
 {
